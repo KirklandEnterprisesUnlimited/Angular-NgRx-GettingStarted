aa9f55673bc4f0276657e0e7c301ea62bda46250
Stephen Kirkland <stephen.kirkland@facilitateenternest.onmicrosoft.com>
2023-04-18 15:23:24 -0400

    About to do name changes

--- APM-Demo0/src/app/products/state/product.reducer.ts
+++ APM-Demo0/src/app/products/state/product.reducer.ts
@@ -1,7 +1,18 @@
 import { createAction, createReducer, on } from "@ngrx/store";
+import { Product } from "../product";
 import { ProductActions } from "./product.actions";
 
-export const productReducer = createReducer({ showProductCode: false }, on(createAction('[Product] Toggle Product Code'), state => {
+export interface State {
+    products: ProductState;
+}
+
+export interface ProductState {
+    showProductCode: boolean;
+    currentProduct: Product;
+    products: Product[];
+}
+
+export const productReducer = createReducer({ showProductCode: false } as ProductState, on(createAction('[Product] Toggle Product Code'), state => {
     return {
         ...state,
         showProductCode: !state.showProductCode
